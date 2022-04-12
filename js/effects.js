be807e4a91ac6a0e7d4fb663605b24ef399815e9
AntonSuslov <87482005+Heymoost@users.noreply.github.com>
2022-04-13 03:12:24 +0400

    Помощь друга

---
+++ js/effects.js
@@ -0,0 +1,100 @@
+const preview = document.querySelector('.preview');
+const onListItemClick = document.querySelector('.effects__list');
+const slider = document.querySelector('.effect-level__slider');
+const sliderForm = document.querySelector('.img-upload__effect-level');
+
+const effectsConfig = {
+  none: {
+    sliderOptions: {
+      start: 0,
+      range: {
+        min: 0,
+        max: 1
+      },
+      step: 0.1
+    },
+    getFilterValue: () => ''
+  },
+  chrome: {
+    sliderOptions: {
+      start: 0,
+      range: {
+        min: 0,
+        max: 1
+      },
+      step: 0.1
+    },
+    getFilterValue: (value) => `grayscale(${value})`
+  },
+  sepia: {
+    sliderOptions: {
+      start: 0,
+      range: {
+        min: 0,
+        max: 1
+      },
+      step: 0.1
+    },
+    getFilterValue: (value) => `sepia(${value})`
+  },
+  marvin: {
+    sliderOptions: {
+      start: 0,
+      range: {
+        min: 0,
+        max: 100
+      },
+      step: 1
+    },
+    getFilterValue: (value) => `invert(${value}%)`
+  },
+  phobos: {
+    sliderOptions: {
+      start: 0,
+      range: {
+        min: 0,
+        max: 3
+      },
+      step: 0.1
+    },
+    getFilterValue: (value) => `blur(${value}px)`
+  },
+  heat: {
+    sliderOptions: {
+      start: 0,
+      range: {
+        min: 1,
+        max: 3
+      },
+      step: 0.1
+    },
+    getFilterValue: (value) => `brightness(${value})`
+  }
+};
+
+noUiSlider.create(slider, effectsConfig.none.sliderOptions);
+
+slider.noUiSlider.on('update', (values) => {
+  const currentEffectName = document.querySelector('.effects__list input:checked').value;
+  const createFilterValue = effectsConfig[currentEffectName].getFilterValue;
+  preview.style.filter = createFilterValue(values[0]);
+  if (currentEffectName === 'none') {
+    sliderForm.classList.add('hidden');
+  }
+  if (currentEffectName !== 'none') {
+    sliderForm.classList.remove('hidden');
+  }
+});
+
+onListItemClick.addEventListener('change', (e) => {
+  const effectName = e.target.value;
+  preview.className = '';
+  preview.classList.add(`effects__preview--${effectName}`);
+  const options = effectsConfig[effectName].sliderOptions;
+  preview.value = effectName;
+  slider.noUiSlider.updateOptions(
+    options,
+    true
+  );
+});
+
---
+++ js/value-element.js
@@ -0,0 +1,27 @@
+const smallerElement = document.querySelector('.scale__control--smaller');
+const valueElement = document.querySelector('.scale__control--value');
+const biggerElement = document.querySelector('.scale__control--bigger');
+const valueStylePicturie = document.querySelector('.img-upload__preview');
+
+valueElement.value = `${100}%`;
+
+const MAX_SCALE = 100;
+const MIN_SCALE = 25;
+
+biggerElement.addEventListener('click', ()=> {
+  let nextValue = parseInt(valueElement.value, 10) + 25;
+  if (nextValue > MAX_SCALE) {
+    nextValue = MAX_SCALE;
+  }
+  valueStylePicturie.style.transform=`scale(${nextValue/100})`;
+  valueElement.value = `${nextValue}%`;
+});
+
+smallerElement.addEventListener('click', ()=> {
+  let previousValue = parseInt(valueElement.value, 10) - 25;
+  if (previousValue < MIN_SCALE) {
+    previousValue = MIN_SCALE;
+  }
+  valueStylePicturie.style.transform=`scale(${previousValue/100})`;
+  valueElement.value = `${previousValue}%`;
+});
