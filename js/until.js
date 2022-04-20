49c9fbc856000e945bd3d06b137e558b8ed6cb06
AntonSuslov <87482005+Heymoost@users.noreply.github.com>
2022-04-20 16:46:53 +0400

    Правки

--- js/until.js
+++ js/until.js
@@ -1,14 +1,5 @@
-const getRandomInt = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = (array) => array[getRandomInt(0, array.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const isEnterKey = (evt) => evt.key === 'Enter';
 
-export {getRandomArrayElement, getRandomInt, isEscapeKey, isEnterKey};
+export {isEscapeKey, isEnterKey};
